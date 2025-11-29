26ae07fc1ec04e8e55f7278c77f3bc84fe1d9e3a
allter <adam.nowak789@onet.pl>
2025-11-29 17:56:16 +0100

    update: redesign for new informations

--- src/components/Scrappers.tsx
+++ src/components/Scrappers.tsx
@@ -3,13 +3,22 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowBigRightDashIcon } from "lucide-react";
+import { ArrowBigRightDashIcon, Clock, FileText, MapPin } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type Offer = {
   id: string;
   name: string;
   salary: string | null;
   link: string;
+  city: string | null;
+  availability: string | null;
+  contractType: string | null;
 };
 
 type DataType = {
@@ -29,7 +38,7 @@ const Scrappers = () => {
 
     try {
       const res = await fetch(
-        `https://bs-scrapper-be.onrender.com/data?provider=${provider}`,
+        `${process.env.NEXT_PUBLIC_FETCH_ADDRESS}/data?provider=${provider}`,
         {
           method: "GET",
           headers: {
@@ -77,29 +86,63 @@ const Scrappers = () => {
             <h3 className="text-xl font-bold place-self-start mb-1">
               {data?.title}
             </h3>
-            <ul className="grid grid-cols-1 sm:grid-cols-3 w-full gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-3 w-full gap-4">
               {data?.offers.map((offer) => (
-                <li
+                <Accordion
+                  type="single"
+                  collapsible
+                  className="w-full"
                   key={offer.id}
-                  className="bg-gray-100 rounded p-2 flex flex-col justify-between gap-4 shadow-md hover:shadow-lg hover:scale-105 hover:cursor-default duration-200 transition-all group"
                 >
-                  <div className="flex">
-                    <p className="font-bold w-2/3">{offer.name}</p>
-                    {offer.salary && (
-                      <span className="text-sm w-1/3">{offer.salary}</span>
-                    )}
-                  </div>
-                  <Link
-                    href={offer.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex gap-1 self-end group-hover:text-blue-500"
-                  >
-                    Przejdź do oferty <ArrowBigRightDashIcon />
-                  </Link>
-                </li>
+                  <AccordionItem value="#">
+                    <AccordionTrigger className="bg-gray-100 rounded p-2 flex flex-col justify-between gap-4 shadow-md hover:shadow-lg hover:scale-105 hover:cursor-default duration-200 transition-all group">
+                      <div className="flex">
+                        <p
+                          className={`font-bold ${
+                            offer.salary ? "w-2/3" : "w-full"
+                          }`}
+                        >
+                          {offer.name}
+                        </p>
+                        {offer.salary && (
+                          <span className="text-sm w-1/3">{offer.salary}</span>
+                        )}
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent className="flex flex-col gap-4 text-balance bg-gray-100 p-4 rounded-b">
+                      <div className="text-sm font-medium gap-1 flex flex-col">
+                        {offer.city && (
+                          <p className="flex items-center gap-1">
+                            <MapPin />
+                            {offer.city}
+                          </p>
+                        )}
+                        {offer.availability && (
+                          <p className="flex items-center gap-1">
+                            <Clock />
+                            {offer.availability}
+                          </p>
+                        )}
+                        {offer.contractType && (
+                          <p className="flex items-center gap-1">
+                            <FileText />
+                            {offer.contractType}
+                          </p>
+                        )}
+                      </div>
+                      <Link
+                        href={offer.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex gap-1 self-end group-hover:text-blue-500"
+                      >
+                        Przejdź do oferty <ArrowBigRightDashIcon />
+                      </Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               ))}
-            </ul>
+            </div>
           </div>
         )}
 
